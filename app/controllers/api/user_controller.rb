2a2308fe3dff6293722b208f27cdbacbd15be2ba
sarthaksoni25 <sarthak.soni@iiitb.org>
2018-03-24 16:17:25 +0530

    Fix @4

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -1,9 +1,15 @@
 class Api::UserController < ApplicationController
     before_action :authenticate_user!
   def index
-      @alluser = User.all
-    render json: {
-      @alluser
-    }, status: 200
+      alluser = User.all
+      @users = []
+      for user in alluser
+
+          @role = user.roles.first.name
+          user = JSON.parse(user.to_json)
+          user["role"]= @role
+          @users.push(user)
+      end
+      render json: @users
   end
 end
