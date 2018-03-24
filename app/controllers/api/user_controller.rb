abbdd34c95318092e3308255eafaf4c1e0fd8d5c
sarthaksoni25 <sarthak.soni@iiitb.org>
2018-03-24 19:04:09 +0530

    Fix @5

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -1,7 +1,21 @@
 class Api::UserController < ApplicationController
     load_and_authorize_resource
     before_action :authenticate_user!
-  def index
+      def users
+        if(checkAuthorityOver(current_user.id , params[:id]))
+            @groupuser = GroupUser.where(user_id: params[:id])
+            @role = user.roles.first.name
+            for g in @groupuser
+                group = Group.find(g.group_id)
+                if group.isDepartment
+                    user["department"] = group.name
+                end
+            end
+            user["role"]= @role
+            render User.find(params[:id])
+        end
+
+      def index
       alluser = User.all
       @users = []
       for user in alluser
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,27 @@
 class User < ApplicationRecord
   rolify
   after_create :assign_default_role
+  def self.checkAuthorityOver(user_id , request_id)
+        @groups=[]
+        @authority_over = []
+        @authority=[]
+           @groups=GroupUser.where(user_id: current_user.id)
+           for group in @groups
+             @groupgroups=GroupGroup.where(groupId_id: group.id)
+             for groupgroup in @groupgroups
+               @authority.push(groupgroup.authorityOver_id)
+             end
+           end
+        for g in authority
+            @authority_over = GroupUser.where(group_id: g.id)
+            for users in @authority_over
+                if(request_id == users.id)
+                    return true
+                end
+            end
+        end
+        return false
+    end
 
   def assign_default_role
     self.add_role(:employee) if self.roles.blank?
