782a05113b3a1838d5886ea137c6c2d12a0ae6d7
sarthaksoni25 <sarthak.soni@iiitb.org>
2018-03-28 11:45:55 +0530

    Add fix @11

--- app/controllers/api/groupgroup_controller.rb
+++ app/controllers/api/groupgroup_controller.rb
@@ -1,5 +1,6 @@
 class Api::GroupgroupController < ApplicationController
   before_action :authenticate_user!
+  require 'set'
 
   def create
     @curr_group=params[:group_id]
@@ -14,4 +15,19 @@ class Api::GroupgroupController < ApplicationController
     end
     render json: GroupGroup.all,status: 200
   end
+
+  def authorityover
+      @groups=[]
+      @authority_over = []
+      @authority=[]
+         @groups=GroupUser.where(user_id: params[:id])
+     for group in @groups
+       @groupgroups=GroupGroup.where(groupId_id: group.id)
+       for groupgroup in @groupgroups
+         @authority.push(groupgroup.authorityOver_id)
+       end
+    end
+    render json: @authority.to_set,status: 200
+  end
+
 end
