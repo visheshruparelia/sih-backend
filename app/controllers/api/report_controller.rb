96fa034970d9a32936e5b1d721180dd4cc8f2ff1
sarthaksoni25 <sarthak.soni@iiitb.org>
2018-03-30 09:21:53 +0530

    Temporary change for lost files(Under deevlopment)

--- app/controllers/api/report_controller.rb
+++ app/controllers/api/report_controller.rb
@@ -17,8 +17,13 @@ class Api::ReportController < ApplicationController
 
     @totalfileslost=0
     @files_lost=[]
+    @lostfiledata=Hash.new
     @file_lost_rows=History.where(changed_by_id: @id,status_to: 3)
     @file_lost_rows.each do |row|
+      @file_prev_rows=History.where(file_id: row.file_id)
+      @file_prev_rows.pop()
+      prevrow = @file_prev_row.pop()
+      @files_lost_between.push([prevrow.changed_by_id,@id])
       @totalfileslost+=1
       @files_lost.push([row.file_id,Allfile.find(row.file_id).name])
     end
@@ -30,10 +35,10 @@ class Api::ReportController < ApplicationController
       @totalfileslegalhold+=1
       @files_legalhold.push([row.file_id,Allfile.find(row.file_id).name])
     end
-
     @report.store('TotalFilesTransferred',@totalfilestransferred)
     @report.store('FilesTransferred',@files_transferred)
 
+    @report.store('filesLostBetween', @files_lost_between)
     @report.store('TotalFilesLost',@totalfileslost)
     @report.store('FilesLost',@files_lost)
 
